340cfe5afa2b80fe32c3ea8ec180e1346e369b63
Danila Safargaliev <SafarDaF@yandex.ru>
2023-02-26 18:51:37 +0300

    Создал два вектора

--- ExploreSTL.cpp
+++ ExploreSTL.cpp
@@ -1,15 +1,29 @@
 ﻿#include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
 
+/*
+Библиотека стандартных шаблонов (STL)
+VECTOR
+*/
+
+
 int main()
 {
+    setlocale(LC_ALL, "ru");
     int a, b, c;
     int array[10]{ 0,1,2,3,4,5,6,7,8,9 };
     char b = 'b';
     string str = "Hello world!";
+    vector<int> intVector;
+    vector<char> charVector;
+
+
+
+
     return 0;
 }
 
