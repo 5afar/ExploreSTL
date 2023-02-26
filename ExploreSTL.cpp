e58098aaee31ed56de1a95789cd6559d40069eef
Danila Safargaliev <SafarDaF@yandex.ru>
2023-02-26 18:30:04 +0300

    Добавил несколько разных переменных

--- ExploreSTL.cpp
+++ ExploreSTL.cpp
@@ -1,12 +1,15 @@
 ﻿#include <iostream>
+#include <string>
 
-
-
+using namespace std;
 
 
 int main()
 {
-    cout << "Hello World!\n";
+    int a, b, c;
+    int array[10]{ 0,1,2,3,4,5,6,7,8,9 };
+    char b = 'b';
+    string str = "Hello world!";
     return 0;
 }
 
