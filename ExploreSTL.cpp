be07b74bb0a5a16a1eea031fe639a94dbf50d794
Danila Safargaliev <SafarDaF@yandex.ru>
2023-02-26 18:58:16 +0300

    Добавил элементы в конец вектора и вывел их на экран

--- ExploreSTL.cpp
+++ ExploreSTL.cpp
@@ -8,6 +8,7 @@ using namespace std;
 /*
 Библиотека стандартных шаблонов (STL)
 VECTOR
+
 */
 
 
@@ -16,10 +17,17 @@ int main()
     setlocale(LC_ALL, "ru");
     int a, b, c;
     int array[10]{ 0,1,2,3,4,5,6,7,8,9 };
-    char b = 'b';
     string str = "Hello world!";
     vector<int> intVector;
     vector<char> charVector;
+    intVector.push_back(7);
+    intVector.push_back(9);
+    intVector.push_back(3);
+    for (int i = 0; i < intVector.size(); i++) {
+        cout << i + 1 <<". " << intVector[i] << endl;
+        int s = intVector[i] + 3;
+        cout << "S = " << s << endl;
+    }
 
 
 
